4c2ca58195445677740f4af99c7503f3c9f8696c
Barketson <50872144+Barketson@users.noreply.github.com>
2019-05-21 16:52:15 +0300

    refactoring script.js from unit1

---
+++ Unit 1/scripts/script.js
@@ -0,0 +1,43 @@
+function userProfile() {
+  var userSurname,
+      userName,
+      userLastname,
+      userAge,
+      userGender,
+      isUserRetired;
+
+  do {
+    userSurname = prompt('Ваша фамилия?', '');
+  } while (+userSurname == 0);
+
+  do {
+    userName = prompt('Ваше имя?', '');
+  } while (+userName == 0);
+
+  do {
+    userLastname = prompt('Ваше отчество?', '');
+  } while (+userLastname == 0);
+
+  do {
+    userAge = +prompt('Ваш возраст?', '');
+  } while (isNaN(userAge) || (userAge == 0));
+
+  userGender = confirm('Ваш пол - женский?') ? 'Женский' : 'Мужской';
+
+  switch (userGender) {
+    case ('Женский'):
+      isUserRetired = (userAge < 60) ? 'нет' : 'да';
+      break;
+    case ('Мужской'):
+      isUserRetired = (userAge < 65) ? 'нет' : 'да';
+  }
+
+  alert('Ваша ФИО : ' + ' ' + userSurname + ' ' + userName + ' ' + userLastname +
+        '\nваш возраст в годах: ' + userAge +
+        '\nваш возраст в днях: ' + (userAge * 365) +
+        '\nчерез 5 лет вам будет: ' + (userAge + 5) +
+        '\nваш пол - ' + userGender +
+        '\nвы на пенсии: ' + isUserRetired);
+}
+
+userProfile();
